8489cc5da21294e3f7eba33aabc3b4ec91f68a5e
DrorGaon <drordtuii@gmail.com>
2024-09-16 17:27:31 +0300

    Improve home component

--- cmps/Home.jsx
+++ cmps/Home.jsx
@@ -1,10 +1,11 @@
-import { UserPreview } from './UserPreview.jsx'
-
 export function Home() {
     return (
         <section>
-            <h2>Home Sweet Home</h2>
-            <UserPreview />
+            <h2>Welcome to Miss Books</h2>
+            <p>Your go to place for anything books!</p>
+            <p>New - now also on the samsung smart fridge, <br />
+            samsung smart toaster and more!</p>
+            <button>Our catalog</button>
         </section>
     )
 }
