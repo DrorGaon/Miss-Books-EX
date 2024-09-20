fab9a9007d3b31f92929fd3c6967276725d75f82
DrorGaon <drordtuii@gmail.com>
2024-09-20 15:50:07 +0300

    Add color based on price

--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -22,11 +22,12 @@ export function BookDetails({bookId, onBack}){
     const pageCountTxt = (pageCount > 500) ? 'Serious Reading' : (pageCount > 200) ? 'Descent Reading' : 'Light Reading'
     const year = new Date().getFullYear()
     const publishedDateTxt = (year - publishedDate >= 10) ? '(Vintage)' :  (year - publishedDate <= 1) ? '(New)' : ''
+    const priceClass = (listPrice.price > 150) ? 'expensive' : (listPrice.price < 20) ? 'cheap' : ''
 
     return (
         <section className="book-details">
             <h2>Title: {title}</h2>
-            <h2>Price: {listPrice.price}{listPrice.symbol}</h2>
+            <h2>Price: <span className={priceClass}>{listPrice.price}{listPrice.symbol}</span></h2>
             <h2>Length: {pageCount} pages {`(${pageCountTxt})`}</h2>
             <h2>Book summary:</h2>
             <p>{desc}</p>
